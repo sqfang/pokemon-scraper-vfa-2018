8c682d9ebefbf46eb83df7ad01ac168c2e02677b
Sophia Fang <sophiaqfang@gmail.com>
2018-06-30 10:03:38 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,6 +1,6 @@
 class Pokemon
   db = SQLite3::Database.new('db/pokemon.db')
   db.execute("DROP TABLE IF EXISTS pokemon")
-  sql_runner = SQLRunner.initialize(db)
-  sql_runner.execute_schema_migration_sql
+  #sql_runner = SQLRunner.initialize(db)
+  #sql_runner.execute_schema_migration_sql
 end
